fe6a64fecec9df13d602fc067dbcabd87956f62c
Andrea Giardina <andrea@hoverstate.com>
2017-01-02 19:09:39 +0100

    utils module first commit

---
+++ src/utils.rkt
@@ -0,0 +1,82 @@
+#lang racket/base
+
+(require charterm)
+(require racket/system)
+
+(define (sh command [param #f])
+  (if (string? param)
+    (system* (find-executable-path command) param)
+    (system* (find-executable-path command))
+    ))
+
+(define (menu items n-active)
+  (let ([active (list-ref items n-active)])
+    (map
+      (lambda (item)
+        (if (eqv? item active)
+          (printf "  \033[36mο\033[0m ~a\033[0m\n" item)
+          (printf "    \033[90m~a\033[0m\n" item)))
+        items)))
+
+(define (show-menu title items n-active)
+  (with-charterm (charterm-clear-screen))
+
+  (printf "~a\n\n" title)
+  (menu items n-active)
+
+  (let ([char (with-charterm (charterm-read-key))]
+        [prev (if (> n-active 0) (- n-active 1) 0)]
+        [next (if (= n-active (- (length items) 1))
+                  (- (length items) 1)
+                  (+ n-active 1))])
+
+    (cond
+      [(eqv? char 'down) (show-menu title items next)]
+      [(eqv? char 'up) (show-menu title items prev)]
+      [(eqv? char 'return) (list-ref items n-active)])))
+
+
+(define (multichoice-menu items active selected)
+  (let ([checked "\033[36m[\u2713]"]
+        [unchecked "\033[90m[ ]"]
+        [text "\033[90m"]
+        [text-active "\033[0m"])
+
+  (for ([i (in-naturals)]
+        [item items])
+        (if (member i selected)
+          (printf "  ~a" checked)
+          (printf "  ~a" unchecked))
+
+        (if (eqv? i active)
+          (printf " ~a~a \033[0m\n" text-active item)
+          (printf " ~a~a \033[0m\n" text item)))))
+
+(define (toggle el li)
+  (if (member el li) (remv el li) (cons el li)))
+
+(define (select idx l)
+  (foldr (lambda (id ret) (cons (list-ref l id) ret)) '() idx))
+
+(define (show-multichoice-menu title items active selected)
+  (with-charterm (charterm-clear-screen))
+
+  (printf "~a\n\n" title)
+  (multichoice-menu items active selected)
+
+  (let ([char (with-charterm (charterm-read-key))]
+        [prev (modulo (- active 1) (length items))]
+        [next (modulo (+ active 1) (length items))])
+
+    (cond
+      [(eqv? char 'down) (show-multichoice-menu title items next selected)]
+      [(eqv? char 'up) (show-multichoice-menu title items prev selected)]
+      [(eqv? char #\space) (show-multichoice-menu title items active (toggle active selected))]
+      [(eqv? char 'return) (select (sort selected <) items)])))
+
+(define (release-name name)
+  (cond [re]))
+
+;(select '(1 2) '("a" "b" "c"))
+;(show-menu "Scegli quello che vuoi?" (list "ciao" "come" "stai") 0)
+;(show-multichoice-menu "Quale saluto preferisci?" (list "ciao" "come stai" "buongiorno") 0 '(0))
